96dac77facdb474503236b9d6bd578c8b16bd73e
unknown <ysoh1123@naver.com>
2018-06-18 14:58:07 +0900

    first version

---
+++ src/B.java
@@ -0,0 +1,11 @@
+public class B {
+	public B() {}
+	public B(int i) {
+		i = i + 1;
+	}
+	public void println() {
+		System.out.println(i);
+		System.out.println("first version");
+	}
+	
+}
---
+++ src/C.java
@@ -0,0 +1,10 @@
+public class C 
+	public C() {}
+	public C(int i) {
+		i = i - 1;
+	}
+	public void println() {
+		System.out.println(i);
+		System.out.println("first version");
+	}
+}
