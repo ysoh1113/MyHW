e1a0b4aba85a10ac00fe3ddcf9bcd252c152cfdc
unknown <ysoh1123@naver.com>
2018-06-18 15:00:52 +0900

    DevB

--- src/A.java
+++ src/A.java
@@ -1,11 +1,11 @@
 public class A {
 	public static void main(String[] args) {
-		
+
 		B b = new B(1);
 		b.println();
 		C c = new C(2);
 		c.println();
-		System.out.println("first version_A.java");
-		
+		System.out.println("DevB_A.java");
+
 	}
 }
